a9b12f546b2ec0792ff1c7c3523eb1bb93f6e7ef
Antonio Vázquez Montalban <antuane.vazquez04@gmail.com>
2024-10-14 17:45:28 -0600

    base components

---
+++ src/components/FormTask.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Task } from './Task'
+
+export const FormTask = () => {
+  return (
+    <div>FormTask</div>
+  )
+}
---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Task } from './Task'
+
+export const ToDoList = () => {
+  return (
+    <div>ToDoList</div>
+  )
+}
